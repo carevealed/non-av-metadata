9a113f2cc9ec5c7eb3a8f5d60fb5ed39decc694e
henryborchers <henryborchers@yahoo.com>
2015-07-09 17:28:51 -0700

    init commit

---
+++ Code/nonAV/Model/CAssetPart.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+from Model import CInstantiations
+from Model import CAssets
+
+class CAssetPart(object):
+	def __init__(self):
+		self.__cInstantiations = None
+		"""@AttributeType Model.CInstantiations*"""
+		self.unnamed_CAssets_ = None
+		# @AssociationType Model.CAssets
+		self.unnamed_CInstantiations_ = None
+		# @AssociationType Model.CInstantiations
+		# @AssociationKind Composition
+
---
+++ Code/nonAV/Model/CAssets.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+from Model import Element
+from Model import CAssetPart
+from Model import CDescriptionDocument
+from Model import CNode
+
+class CAssets(CNode):
+	def __make_xml(self):
+		pass
+
+	def __init__(self):
+		self.__objectID = None
+		"""@AttributeType Model.Element"""
+		self.__projectID = None
+		"""@AttributeType Model.Element"""
+		self.__physicalLocation = None
+		"""@AttributeType Model.Element"""
+		self.__dimensionsHeight = None
+		"""@AttributeType Model.Element"""
+		self.__dimensionsWidth = None
+		"""@AttributeType Model.Element"""
+		self.__color = None
+		"""@AttributeType Model.Element"""
+		self.__hasParts = None
+		"""@AttributeType Model.Element"""
+		self.__additionalDescription = None
+		"""@AttributeType Model.Element"""
+		self.__assetPart = None
+		"""@AttributeType Model.CAssetPart"""
+		self.unnamed_CDescriptionDocument_ = None
+		# @AssociationType Model.CDescriptionDocument
+		self.unnamed_CAssetPart_ = None
+		# @AssociationType Model.CAssetPart
+		# @AssociationKind Composition
+
