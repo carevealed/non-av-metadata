60d5cfcaba69481ea034338d5ff6c81e2c5ed66b
henryborchers <henryborchers@yahoo.com>
2015-07-13 09:36:44 -0700

    Added html documentation

---
+++ Code/nonAV/Model/Cinstantiation.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+from Model import Element
+from Model import CTechnical
+from Model import CInstantiations
+from Model.CNode import CNode
+
+class CInstantiation(CNode):
+    def __make_xml(self):
+        pass
+
+def __init__(self):
+    self.__fileName = None
+    """@AttributeType Model.Element"""
+    self.__fileSize = None
+    """@AttributeType Model.Element"""
+    self.__checksum = None
+    """@AttributeType Model.Element"""
+    self.__deritiveFrom = None
+    """@AttributeType Model.Element"""
+    self.__technical = None
+    """@AttributeType Model.CTechnical"""
+    self.__attributes = None
+    """@AttributeType OrderedDict"""
+    self.unnamed_CInstantiations_ = None
+    # @AssociationType Model.CInstantiations
+    self.unnamed_CTechnical_ = None
+    # @AssociationType Model.CTechnical
+    # @AssociationKind Composition
+
