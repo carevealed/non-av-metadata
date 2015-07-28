c1e243d80e552da41bc4836ac85071352e4dff09
henryborchers <henryborchers@yahoo.com>
2015-07-28 14:31:54 -0700

    DescriptiveDocument class now produces XML

--- Code/nonAV/Model/DescriptionDocument.py
+++ Code/nonAV/Model/DescriptionDocument.py
@@ -3,26 +3,50 @@
 from Model import DublinCore
 import Model.Assets
 from Model import CollectionReference
+from Model import Element
 from Model import CAPS_node
 
 class DescriptionDocument(CAPS_node):
     def __init__(self):
-        self.__dublinCore = None
-        """@AttributeType Model.DublinCore"""
-        self.__assets = None
-        """@AttributeType Model.Assets"""
-        self.unnamed_CollectionReference_ = None
-        # @AssociationType Model.CollectionReference
-        self.unnamed_DublinCore_ = None
-        # @AssociationType Model.DublinCore
-        # @AssociationKind Composition
-        self.unnamed_Assets_ = None
-        # @AssociationType Model.Assets
-        # @AssociationKind Composition
+        """
 
-    def __make_xml(self):
+        :return:
+        """
+        super(DescriptionDocument, self).__init__()
+        self._dublinCore = None
+        self._assets = None
+
+    def _make_xml(self):
+        root = Element("DescriptionDocument")
+        root.add_child(self.dublinCore)
+        root.add_child(self.assets)
+        return root
+
+    def _check_required(self):
+        missing_fields = []
+        if not self.dublinCore:
+            missing_fields.append("dublinCore")
+        if not self.assets:
+            missing_fields.append("assets")
+        if len(missing_fields) > 0:
+            raise Exception("Missing required metadata fields, '" + "', '".join(missing_fields) + "'.")
         pass
 
     def validate_attribute(self):
         pass
 
+    @property
+    def dublinCore(self):
+        return self._dublinCore
+
+    @dublinCore.setter
+    def dublinCore(self, value):
+        self._dublinCore = value
+
+    @property
+    def assets(self):
+        return self._assets
+
+    @assets.setter
+    def assets(self, value):
+        self._assets = value
